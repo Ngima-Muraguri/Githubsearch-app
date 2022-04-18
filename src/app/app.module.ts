20af8be6aced6f2d060a47e771832ee8228a361c
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-04-18 11:42:28 +0300

    ft;created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileAppComponent } from './profile-app/profile-app.component';
+import { ProfileDataComponent } from './profile-data/profile-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileAppComponent,
+    ProfileDataComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/profile-app/profile-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-app',
+  templateUrl: './profile-app.component.html',
+  styleUrls: ['./profile-app.component.css']
+})
+export class ProfileAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
