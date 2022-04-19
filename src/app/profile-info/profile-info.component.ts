a38d6d0347274bd6ce2cb4803b89b4914236aab4
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-04-19 14:42:43 +0300

    st;added user details

--- src/app/profile-info/profile-info.component.ts
+++ src/app/profile-info/profile-info.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileInfoComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() { 
+
+  }
+  
 
   ngOnInit(): void {
   }
